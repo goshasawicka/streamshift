51c8a870807322c47e06d75198f3d08fda08b8df
Gosha Galazka <mal.galazka@gmail.com>
2016-11-28 22:29:13 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
     name="streamshift",
     version="0.0.2",
@@ -18,6 +11,5 @@ setup(
     keywords="radio audio stream",
     url="https://github.com/patyk/streamshift",
     packages=['streamshift', 'tests'],
-    long_description=read('README.md'),
     classifiers=[],
 )
