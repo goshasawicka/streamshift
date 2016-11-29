93f4999b8480ba565e002665d4001a0b3b1cff33
gosha <mal.galazka@gmail.com>
2016-11-28 23:08:35 -0800

    Script path

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 setup(
     name="streamshift",
-    version="0.0.6",
+    version="0.0.7",
     author="Gosha Sawicka",
     author_email="mal.galazka@gmail.com",
-    scripts=['bin/streamshift.py'],
+    scripts=['bin/streamshift'],
     description=("Buffer audio streams and serve them back with a user-defined offset"),
     license="MIT",
     keywords="radio audio stream",
