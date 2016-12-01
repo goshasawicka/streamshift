78d11be2906396dff6730ee30ebba9f2d51bd610
Gosha Sawicka <mal.galazka@gmail.com>
2016-11-30 20:36:39 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="streamshift",
-    version="0.0.7",
+    version="0.0.8",
     author="Gosha Sawicka",
     author_email="mal.galazka@gmail.com",
     description=("Buffer audio streams and serve them back with a user-defined offset"),
